2996d04c20b3606eeee24f5726cf7570efb246be
zhangxiaode <2279323443@qq.com>
2019-12-06 07:57:42 +0800

    优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,74 +1,72 @@
 import { request } from "../../utils/index";
 Page({
-  data: {
-    displayMultipleItems: 4,
-    nav: [],
-    categoryId: 1,
-    pageIndex: 1,
-    pageSize: 10,
-    videos: []
-  },
-  onShow() {
-    this.getCategory();
-  },
-  onPullDownRefresh() {
-    // 下拉刷新操作
-    this.setData({
-      pageIndex: 1
-    })
-    this.getVideo();
-  },
-  onReachBottom() {
-    // 上拉加载操作
-    this.setData({
-      pageIndex: this.data.pageIndex + 1
-    })
-    this.getVideo();
-  },
-  changeNav(e) {
-    this.setData({
-      categoryId: e.currentTarget.dataset.id
-    })
-    this.getVideo();
-  },
-  getCategory() {
-    request({
-      url: `/apis/category`,
-      method: "get",
-      data: {
-      }
-    }).then(res => {
-      if (res.code == 200) {
+    data: {
+        displayMultipleItems: 4,
+        nav: [],
+        categoryId: 1,
+        pageIndex: 1,
+        pageSize: 10,
+        videos: []
+    },
+    onShow() {
+        this.getCategory();
+    },
+    onPullDownRefresh() {
+        // 下拉刷新操作
         this.setData({
-          nav: res.data
+            pageIndex: 1
         })
         this.getVideo();
-      }
-    })
-  },
-  getVideo() {
-    request({
-      url: `/apis/video`,
-      method: "get",
-      data: {
-        pageIndex: this.data.pageIndex,
-        pageSize: this.data.pageSize,
-        categoryId: this.data.categoryId
-      }
-    }).then(res => {
-      if (res.code == 200) {
-        var videos = this.data.videos;
-        if (this.data.pageIndex == 1) {
-          videos = [];
-        }
+    },
+    onReachBottom() {
+        // 上拉加载操作
         this.setData({
-          videos: [...videos, ...res.data]
+            pageIndex: this.data.pageIndex + 1
         })
-        console.log(this.data.videos)
-        wx.stopPullDownRefresh();
-      }
-    })
-  },
-  goVideoDetail() {
-  }
+        this.getVideo();
+    },
+    changeNav(e) {
+        this.setData({
+            pageIndex: 1,
+            categoryId: e.currentTarget.dataset.id
+        })
+        this.getVideo();
+    },
+    getCategory() {
+        request({
+            url: `/apis/category`,
+            method: "get",
+            data: {}
+        }).then(res => {
+            if (res.code == 200) {
+                this.setData({
+                    nav: res.data
+                })
+                this.getVideo();
+            }
+        })
+    },
+    getVideo() {
+        request({
+            url: `/apis/video`,
+            method: "get",
+            data: {
+                pageIndex: this.data.pageIndex,
+                pageSize: this.data.pageSize,
+                categoryId: this.data.categoryId
+            }
+        }).then(res => {
+            if (res.code == 200) {
+                var videos = this.data.videos;
+                if (this.data.pageIndex == 1) {
+                    videos = [];
+                }
+                this.setData({
+                    videos: [...videos, ...res.data]
+                })
+                wx.stopPullDownRefresh();
+            }
+        })
+    },
+    goVideoDetail() {}
 })
--- utils/index.js
+++ utils/index.js
@@ -1,113 +1,113 @@
 // const baseUrl = 'https://test.ixgoo.cn'
 // const wssUrl = 'wss://test.ixgoo.cn'
-const baseUrl = 'http://localhost:9000'
+const baseUrl = 'http://192.168.5.100:9000'
 const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+    n = n.toString()
+    return n[1] ? n : '0' + n
 }
 
 const getSetting = () => {
-  return new Promise((resolve, reject) => {
-    wx.getSetting({
-      success: (res) => {
-        resolve(res)
-      },
-      fail: (err) => {
-        reject(err)
-      }
+    return new Promise((resolve, reject) => {
+        wx.getSetting({
+            success: (res) => {
+                resolve(res)
+            },
+            fail: (err) => {
+                reject(err)
+            }
+        })
     })
-  })
 }
 const getUserInfo = () => {
-  return new Promise((resolve, reject) => {
-    wx.getUserInfo({
-      success: (res) => {
-        resolve(res)
-      },
-      fail: (err) => {
-        reject(err)
-      }
+    return new Promise((resolve, reject) => {
+        wx.getUserInfo({
+            success: (res) => {
+                resolve(res)
+            },
+            fail: (err) => {
+                reject(err)
+            }
+        })
     })
-  })
 }
 const login = () => {
-  return new Promise((resolve, reject) => {
-    wx.login({
-      success: (res) => {
-        resolve(res)
-      },
-      fail: (err) => {
-        reject(err)
-      }
-    })
-  })
-}
-const request = function ({
-  url,
-  data = {},
-  header = { 'token': wx.getStorageSync('token') || "" },
-  method = "post",
-  dataType = "json",
-  responseType = "text"
-}) {
-  return new Promise((resolve, reject) => {
-    wx.showLoading({
-      title: '加载中'
-    })
-    var headers = "header";
-    var options = {
-      url: baseUrl + url,
-      data,
-      method,
-      dataType,
-      responseType,
-      success: (res) => {
-        wx.hideLoading();
-        if (res.data.code == 200) {
-          resolve(res.data)
-        } else {
-          wx.showToast({
-            title: res.data.msg,
-            complete: () => {
-              resolve(res.data)
+    return new Promise((resolve, reject) => {
+        wx.login({
+            success: (res) => {
+                resolve(res)
+            },
+            fail: (err) => {
+                reject(err)
             }
-          });
-        }
-      },
-      fail: (err) => {
-        wx.hideLoading();
-        wx.showToast({
-          title: '网络错误!',
-          complete: () => {
-            reject(err)
-          }
         })
-      },
-      complete: () => {
-        // hideLoading();
-      }
-    }
-    header.token = wx.getStorageSync('token') || "";
-    options[headers] = header;
-    wx.request(options)
-  })
+    })
+}
+const request = function({
+    url,
+    data = {},
+    header = { 'token': wx.getStorageSync('token') || "" },
+    method = "post",
+    dataType = "json",
+    responseType = "text"
+}) {
+    return new Promise((resolve, reject) => {
+        wx.showLoading({
+            title: '加载中'
+        })
+        var headers = "header";
+        var options = {
+            url: baseUrl + url,
+            data,
+            method,
+            dataType,
+            responseType,
+            success: (res) => {
+                wx.hideLoading();
+                if (res.data.code == 200) {
+                    resolve(res.data)
+                } else {
+                    wx.showToast({
+                        title: res.data.msg,
+                        complete: () => {
+                            resolve(res.data)
+                        }
+                    });
+                }
+            },
+            fail: (err) => {
+                wx.hideLoading();
+                wx.showToast({
+                    title: '网络错误!',
+                    complete: () => {
+                        reject(err)
+                    }
+                })
+            },
+            complete: () => {
+                // hideLoading();
+            }
+        }
+        header.token = wx.getStorageSync('token') || "";
+        options[headers] = header;
+        wx.request(options)
+    })
 }
 
 
 module.exports = {
-  formatTime,
-  getSetting,
-  getUserInfo,
-  login,
-  request
+    formatTime,
+    getSetting,
+    getUserInfo,
+    login,
+    request
 }
