25b81f1718e94bd9e959120167db03b15f8b4d19
xiaode <2279323443@qq.com>
2020-06-01 23:40:50 +0800

    调试

--- pages/index/index.js
+++ pages/index/index.js
@@ -81,7 +81,9 @@ Page({
     },
     goDetail(event) {
         var item = event.currentTarget.dataset.item;
-        if (item.category_id == 9 || item.category_id == 10 || item.category_id == 11) {
+        if (item.category_id > 11) {
+            var url = `/pages/detail/index?url=http://192.168.5.100:9003${item.uri}&name=${item.name}`;
+        } else if (item.category_id == 9 || item.category_id == 10 || item.category_id == 11) {
             var url = `/pages/detail/index?url=http://192.168.5.100:9002${item.uri}&name=${item.name}`;
         } else {
             var url = `/pages/detail/index?url=http://192.168.5.100:9001${item.uri}&name=${item.name}`
